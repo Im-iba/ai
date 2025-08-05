901065ab19d596b1f32982c26b8f24e1e8a8862d
Im-iba <abinayasampath010@gmail.com>
2025-08-05 15:33:13 +0530

    Update test.py with latest changes

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-print("hello world")
-print("howww")
-print("hiiii")
+from collections import deque
+def bfs(graph, strat_node):
+    visited=set()
+    queue=deque()
+    queue.append(strat_node)
+    visited.add(strat_node)
+    print("tranversal of the graph")
+    while queue:
+        current_node=queue.popleft()
+        print(current_node, end=" ")
+
+        for neighbour in graph.get(current_node,[]):
+            if neighbour not in visited:
+                queue.append(neighbour)
+graph={
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':[],
+    'F':[]
+}
+bfs(graph,'A')
